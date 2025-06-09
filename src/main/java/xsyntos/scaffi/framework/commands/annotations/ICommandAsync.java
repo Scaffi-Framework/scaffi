75e7dd1d13894d02ddaa4ff09cea6c18e5643d17
Xsyntos <stevenschool2003@hotmail.com>
2025-06-09 19:15:07 +0200

    Added another change

--- src/main/java/xsyntos/scaffi/framework/commands/annotations/ICommandAsync.java
+++ src/main/java/xsyntos/scaffi/framework/commands/annotations/ICommandAsync.java
@@ -3,6 +3,9 @@ package xsyntos.scaffi.framework.commands.annotations;
 import xsyntos.scaffi.framework.commands.CommandContext;
 import xsyntos.scaffi.framework.commands.CommandResponse;
 
+/**
+ * A Function that returns the temporary CommandResponse while the command is executing
+ */
 @FunctionalInterface
 public interface ICommandAsync {
     public CommandResponse response(CommandContext context);
